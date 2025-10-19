83ea4a9443cb456ca8ce9af11347bc10f56cc01a
Rohit Kalam <126466425+RohitKalam@users.noreply.github.com>
2025-10-19 20:04:21 +0530

    Ready to use Project Template

--- SmartSchoolUI/Index.Designer.cs
+++ SmartSchoolUI/Index.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -42,23 +41,16 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(158, 108);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
-            // 
             // Index
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(467, 318);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Index";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Index_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,7 +59,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
