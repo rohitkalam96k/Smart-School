9c2a9c3ef62ff494171b49c2921014087c1dc4f3
ParshuramPj47 <karan2000005@gmail.com>
2025-11-06 20:44:53 +0530

    Implemeted BLL Methods (#25)
    * RegistrationBLL created
    
    * #10 Implment

--- Models/DocumetSubmittedModel.cs
+++ Models/DocumetSubmittedModel.cs
@@ -8,10 +8,12 @@ namespace Models
     public class DocumetSubmittedModel
     {
         public int documentId { get; set; }
+
         public int academicId { get; set; }
         public string tc { get; set; }
         public string adharcard { get; set; }
         public string oldMarksheet { get; set; }
+
         public DateTime? submissionDate { get; set; }
    
     }
--- Models/FeesModel.cs
+++ Models/FeesModel.cs
@@ -8,11 +8,11 @@ namespace Models
     public class FeesModel
     {
         public int feeId { get; set; }
-        public int? academicId { get; set; }
-        public decimal? totalFees { get; set; }
-        public decimal? paid { get; set; }
-        public decimal? due { get; set; }
-        public string? remarkDue { get; set; }
+        public int academicId { get; set; }
+        public decimal totalFees { get; set; }
+        public decimal paid { get; set; }
+        public decimal due { get; set; }
+        public string remarkDue { get; set; }
         public string paymentStatus { get; set; } 
 
      }
