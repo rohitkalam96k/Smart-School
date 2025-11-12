0e60f543e650d0ba344df40041b8263b89490622
SaulakheS <161022023+SaulakheS@users.noreply.github.com>
2025-11-12 09:43:16 +0530

    updates documentsubmit model

--- Models/DocumetSubmittedModel.cs
+++ Models/DocumetSubmittedModel.cs
@@ -8,19 +8,11 @@ namespace Models
     public class DocumetSubmittedModel
     {
         public int documentId { get; set; }
-<<<<<<< HEAD
-=======
-
->>>>>>> 9c2a9c3ef62ff494171b49c2921014087c1dc4f3
         public int academicId { get; set; }
         public string tc { get; set; }
         public string adharcard { get; set; }
         public string oldMarksheet { get; set; }
-<<<<<<< HEAD
-=======
-
->>>>>>> 9c2a9c3ef62ff494171b49c2921014087c1dc4f3
-        public DateTime? submissionDate { get; set; }
+        public DateTime submissionDate { get; set; }
    
     }
 }
